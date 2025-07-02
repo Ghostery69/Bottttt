aa942a2ccae1c19f8f4a91904bf85899eb6a821b
Ghostery69 <157644164+Ghostery69@users.noreply.github.com>
2025-07-02 01:46:16 +0000

    Update server.py

--- server.py
+++ server.py
@@ -1,14 +1,12 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template
 from flask_cors import CORS
-import os
 
 app = Flask(__name__)
-CORS(app)  # Permet les requêtes cross-origin (utile si frontend séparé)
+CORS(app)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return jsonify({"message": "Hello, Render!"})
+    return render_template("index.html")
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
